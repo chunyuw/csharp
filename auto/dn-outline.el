919a7e9557801b2863cd632fc61454a63e4fdeac
Chunyu Wang <cymacs@gmail.com>
2006-08-20 07:29:36 +0000

    update

---
+++ auto/dn-outline.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "dn-outline"
+ (lambda ()
+    (TeX-run-style-hooks
+     "pgf/dn-framework"
+     "pgf/clr-compile-exec"
+     "pgf/cts-typesystem"
+     "pgf/cts-v-r-types"
+     "pgf/fcl-namespaces")))
+
