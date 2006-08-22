4ae00a3c6a328d7f08c1423f66d7ce8734b3dd81
Chunyu Wang <cymacs@gmail.com>
2006-08-22 02:11:50 +0000

    Batch checkin by Makefile (20060822-10:11)

--- auto/dn-outline.el
+++ auto/dn-outline.el
@@ -1,11 +1,11 @@
 (TeX-add-style-hook "dn-outline"
  (lambda ()
     (TeX-run-style-hooks
-     "pgf/dn-framework"
-     "pgf/clr-compile-exec"
-     "pgf/cts-typesystem"
-     "pgf/cts-v-r-types"
-     "pgf/clr-compile"
-     "pgf/clr-execute"
-     "pgf/fcl-namespaces")))
+     "pgf-dn/dn-framework"
+     "pgf-dn/clr-compile-exec"
+     "pgf-dn/cts-typesystem"
+     "pgf-dn/cts-v-r-types"
+     "pgf-dn/clr-compile"
+     "pgf-dn/clr-execute"
+     "pgf-dn/fcl-namespaces")))
 
