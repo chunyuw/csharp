857ad1b70b2e69339d2505093130913c7ce05b4b
Chunyu Wang <cymacs@gmail.com>
2006-09-30 10:47:02 +0000

    Batch checkin by Makefile (2006-09-30 18:47 on cst)

--- auto/part-08.el
+++ auto/part-08.el
@@ -2,5 +2,17 @@
  (lambda ()
     (TeX-run-style-hooks
      "preamble"
-     "author")))
+     "author"
+     "dn-intro"
+     "dn-outline"
+     "dn-devel"
+     "cs-basic"
+     "cs-class"
+     "cs-advac"
+     "cs-other"
+     "lib-basic"
+     "lib-thread"
+     "lib-stream"
+     "lib-forms"
+     "ado-basic")))
 
